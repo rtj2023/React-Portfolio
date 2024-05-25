488d55f66b664966f7de38269d4fbf3537228831
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-05-24 21:36:32 -0400

    contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,8 @@ function Contact() {
                 <label htmlFor="name">First name:</label><br />
                 <input type="text" id="name" name="name" /><br />
                 <label htmlFor="email">Last name:</label><br />
-                <input type="text" id="email" name="email" />
-                <label htmlFor="message">Last name:</label><br />
+                <input type="text" id="email" name="email" /><br/>
+                <label htmlFor="message">Message:</label><br />
                 <input type="text" id="message" name="message" />
             </form>
 
