565d9eda2839d788bf1a6c89d37b5b9cba58e37a
rtj2023 <jiwanmallrebecca32@gmail.com>
2024-05-24 21:36:22 -0400

    About compononet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,9 @@ function About() {
   return (
     <div className="about">
         <h1>
-            About Me
+            Hi, my name is Rebecca Jiwanmall. I have a Bachelor's degree in Health Services Administration but decided to go a whole new route which may be obvious....coding!
         </h1>
-        <img src="" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScVWdpSX0xv-SPTJEGBorXQzdtwZuCh-XdyA&s" alt="It's me!!" />
         <p>
             This is all about me. Things and stuff
         </p>
